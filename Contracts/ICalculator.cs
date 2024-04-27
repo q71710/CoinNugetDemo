60e4007b5f09d60cfde445e1d1c0958a8f25dba6
quintin <q71710@gmail.com>
2024-04-27 16:02:12 +0800

    add files

---
+++ Contracts/ICalculator.cs
@@ -0,0 +1,10 @@
+namespace CoinNugetDemo.Contracts
+{
+    public interface ICalculator
+    {
+        public int Add(int a, int b);
+        public int Subtract(int a, int b);
+        public int Multiply(int a, int b);
+        public int Divide(int a, int b);
+    }
+}
